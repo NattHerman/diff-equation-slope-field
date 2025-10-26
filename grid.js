b9844b18b2c280357b6017bb5a0076d88e111303
NattHerman <daghermanoyerhamn@gmail.com>
2025-10-26 20:46:04 +0100

    set indent to tabs

--- grid.js
+++ grid.js
@@ -1,59 +1,58 @@
 class Grid {
-  constructor(scale, offset, spacing, count) {
-    // gridspace = screenspace / scale
-    this.scale = scale.copy(); // 2d vector
-    // Spacing between the grid lines in x and y direction
-    this.spacing = spacing.copy(); // 2d vector
-    
-    this.count = count.copy()
-    
-    this.offset = offset.copy()
-  }
-  
-  getPixelPos(x, y) {
-    return createVector(
-      x * this.scale.x + this.offset.x,
-      -y * this.scale.y + this.offset.y
-    )
-  }
-  
-  getWorldPos(x, y) {
-    return createVector(
-      (x - this.offset.x) / this.scale.x,
-      -(y - this.offset.y) / this.scale.y
-    )
-  }
+	constructor(scale, offset, spacing, count) {
+		// gridspace = screenspace / scale
+		this.scale = scale.copy(); // 2d vector
+		// Spacing between the grid lines in x and y direction
+		this.spacing = spacing.copy(); // 2d vector
 
-  draw() {
-    strokeWeight(1)
-    stroke("#707497")
-    // Vertical lines
-    for (let x = 0; x < this.count.x / 2; x ++) {
-      let horizontalPos = x * this.spacing.x * this.scale.x + this.offset.x
-      line(horizontalPos, 0, horizontalPos, height)
-    }
-    for (let x = 0; x > -this.count.x / 2; x --) {
-      let horizontalPos = x * this.spacing.x * this.scale.x + this.offset.x
-      line(horizontalPos, 0, horizontalPos, height)
-    }
-    
-    // Horizontal lines
-    for (let y = 0; y < this.count.y / 2; y++) {
-      let verticalPos = -y * this.spacing.y * this.scale.y + this.offset.y
-      line(0, verticalPos, width, verticalPos)
-    }
-    for (let y = 0; y > -this.count.y / 2; y--) {
-      let verticalPos = -y * this.spacing.y * this.scale.y + this.offset.y
-      line(0, verticalPos, width, verticalPos)
-    }
-    
-    // x-axis
-    strokeWeight(2)
-    stroke("#4E52A8")
-    let origin = this.getPixelPos(0, 0)
-    line(0, origin.y, width, origin.y)
-    line(origin.x, 0, origin.x, height)
-    
-  }
+		this.count = count.copy()
+
+		this.offset = offset.copy()
+	}
+
+	getPixelPos(x, y) {
+		return createVector(
+			x * this.scale.x + this.offset.x,
+			-y * this.scale.y + this.offset.y
+		)
+	}
+
+	getWorldPos(x, y) {
+		return createVector(
+			(x - this.offset.x) / this.scale.x,
+			-(y - this.offset.y) / this.scale.y
+		)
+	}
+
+	draw() {
+		strokeWeight(1)
+		stroke("#707497")
+		// Vertical lines
+		for (let x = 0; x < this.count.x / 2; x ++) {
+			let horizontalPos = x * this.spacing.x * this.scale.x + this.offset.x
+			line(horizontalPos, 0, horizontalPos, height)
+		}
+		for (let x = 0; x > -this.count.x / 2; x --) {
+			let horizontalPos = x * this.spacing.x * this.scale.x + this.offset.x
+			line(horizontalPos, 0, horizontalPos, height)
+		}
+
+		// Horizontal lines
+		for (let y = 0; y < this.count.y / 2; y++) {
+			let verticalPos = -y * this.spacing.y * this.scale.y + this.offset.y
+			line(0, verticalPos, width, verticalPos)
+		}
+		for (let y = 0; y > -this.count.y / 2; y--) {
+			let verticalPos = -y * this.spacing.y * this.scale.y + this.offset.y
+			line(0, verticalPos, width, verticalPos)
+		}
+
+		// x-axis
+		strokeWeight(2)
+		stroke("#4E52A8")
+		let origin = this.getPixelPos(0, 0)
+		line(0, origin.y, width, origin.y)
+		line(origin.x, 0, origin.x, height)
+	}
 }
 
