81148daccd1ebcaaabdeb5a61e192dd1332c314f
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-01 21:43:37 +0100

    Update script with options on-load

--- options.js
+++ options.js
@@ -1,6 +1,11 @@
 let form = document.querySelector("#options");
-form.elements["slope-field"].checked = displaySlopeField
 
+// Update script with options on-load
+displaySlopeField = form.elements["slope-field"].checked
+displayIVPPath = form.elements["IVP-path"].checked
+
+// Update script with options on-change
 form.onchange = (event) => {
     displaySlopeField = form.elements["slope-field"].checked
+    displayIVPPath = form.elements["IVP-path"].checked
 }
